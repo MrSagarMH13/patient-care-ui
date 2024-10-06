4fac2968da8bba4e25e45b5dd1af3a5d5b373c73
Sagar Shinde <sagar@sagar.com>
2024-10-06 09:42:36 +0530

    Remove gitignored files from tracking

--- src/app/core/constant/api.constants.ts
+++
@@ -1,9 +0,0 @@
-export const API_URL = 'http://localhost:8081/api';
-export const PATIENTS_ENDPOINT = {
-  GET_ALL: '/patients/all',
-  SEARCH: '/patients/search',
-  GET_BY_ID: '/patients/:id',
-  CREATE: '/patients/upsert',
-  UPDATE: '/patients/:id',
-  DELETE: '/patients/:id',
-};
